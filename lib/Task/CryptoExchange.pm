2ceefdf4f7eaa730897e703e09b8e1071a91e76a
perlancar (on pc-office) <perlancar@gmail.com>
2018-01-09 08:53:02 +0700

    Initial commit

---
+++ lib/Task/CryptoExchange.pm
@@ -0,0 +1,13 @@
+package Task::CryptoExchange;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Modules that interface to cryptocurrency exchanges
+
+=pkggroup Included modules
+
+#CODE: for (`acme-cpanlists ls-entries "Modules that interface to cryptocurrency exchanges"`) { print "=pkg $_\n" }
+
+=cut
